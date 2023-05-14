c2479b5be85885baa1e6ed46807b5b9273ed7320
ItChyPiCkLe <79584874+ItChyPiCkLe@users.noreply.github.com>
2023-05-14 15:26:41 +0100

    Add files via upload

---
+++ backEnd.php
@@ -0,0 +1,19 @@
+
+
+
+<script src="../assignment/application/js/jquery-3.2.1.js" crossorigin="anonymous"></script>
+<!-- JQuery code to get content data from a backend PHP database  '############# @@@@   -->	  
+<script src="../assignment/application/js/getJsonFromPHPBackEnd.js"></script>
+<!-- ############### @@@@@@@@@ #################@@@@@@@@@@ ############# @@@@   -->	 
+
+<?php
+
+
+
+?>
+
+<html>
+
+
+
+</html>
